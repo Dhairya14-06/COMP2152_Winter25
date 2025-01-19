08d32fb427dce9f7678acf34bca8b29b40b7cff6
Dhairya14-06 <DhairyaAshokbhai.Gohel@georgebrown.ca>
2025-01-19 18:50:12 -0500

    Added lab1.py for Lab 1 submission

---
+++ lab1.py
@@ -0,0 +1,15 @@
+# Defining Variables
+numbers = [1, 4, 7, 9]
+
+# Order of Operations
+a, b, c, d = 1, 2, 3, 4
+# Fully bracketed version
+e = (a - ((b ** c) // d) + (a % c))
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+print("Now showing the shop items filtered by age:", userAge)
